a19e8a6010f166814092750eae88f22dfecb525f
SebastianLai <surrego@nexos-software.com.co>
2025-09-09 15:03:42 -0500

    estructura-inicial-microservicio

---
+++ Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -0,0 +1,27 @@
+﻿namespace Basket.API.Basket.StoreBasket
+{
+    public record StoreBasketCommmand(ShoppingCart Cart) : ICommand<StoreBasketResult>; 
+    public record StoreBasketResult(string UserName); 
+    
+    public class StoreBasketCommandValidator : AbstractValidator<StoreBasketCommmand>
+    {
+        public StoreBasketCommandValidator()
+        {
+            RuleFor(x => x.Cart).NotNull().WithMessage("Cart can not be null");
+            RuleFor(x => x.Cart.UserName).NotEmpty().WithMessage("UserName is required"); 
+        }
+    }
+
+    public class StoreBasketCommandHandler : ICommandHandler<StoreBasketCommmand, StoreBasketResult>
+    {
+        public async Task<StoreBasketResult> Handle(StoreBasketCommmand command, CancellationToken cancellationToken)
+        {
+            ShoppingCart cart = command.Cart;
+
+            //TODO: store basket in database (use Marten upsert - if exist = update, if not = insert) 
+            //TODO: update cache
+
+            return new StoreBasketResult("swn"); 
+        }
+    }
+}
---
+++ Basket.API/Program.cs
@@ -0,0 +1,41 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var assembly = typeof(Program).Assembly;
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LogginBehavior<,>));
+});
+
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!); // connection string desde catalog 
+    opts.Schema.For<ShoppingCart>().Identity(x => x.UserName); // le estamos diciendo que el Id es UserName
+}).UseLightweightSessions(); 
+
+builder.Services.AddCarter();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapCarter(); 
+
+app.Run();
